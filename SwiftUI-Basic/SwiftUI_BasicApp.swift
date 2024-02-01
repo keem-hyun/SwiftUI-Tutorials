e6c58957a9a7b4ef38b697b76262c2938ef62d0e
keem-hyun <hw813@naver.com>
2024-02-01 23:53:49 +0900

    Initial Commit

---
+++ SwiftUI-Basic/SwiftUI_BasicApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicApp.swift
+//  SwiftUI-Basic
+//
+//  Created by 김강현 on 2/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
